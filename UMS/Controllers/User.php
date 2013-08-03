1dd070c5dd927e145aa4480ce9ed5f89823d654a
Jasper Stafleu <j.stafleu@jcgroep.nl>
2013-08-03 22:06:44 +0200

    Backend form validation and notification of errors handled for create user

--- UMS/Controllers/User.php
+++ UMS/Controllers/User.php
@@ -13,6 +13,32 @@ class User
      */
     public static function create()
     {
+        // create a user from the POST variables, if they are valid
+        if ( !empty($_POST) ) {
+            $user = new \UMS\Models\User($_POST);
+            $_POST['errors'] = self::_validate($user);
+
+            // no errors? Save the user, log her in and try context based
+            // handling (usually goes to update the same user)
+            if ( empty($_POST['errors']) ) {
+                $user->save();
+
+                // If no user is already logged in (ie an admin creating a new
+                // account), log the newly created user in
+                if ( !Login::getUser() ) {
+                    Login::doLogin();
+                }
+
+                // ensure the new contextbased action is not triggered using the
+                // just posted values
+                $_POST = array();
+
+                // go to context based action determination (update for new
+                // users, listing for admins
+                return self::contextbased();
+            }
+        }
+
         echo new \UMS\Views\Standard('standard.phtml', $_POST);
     } // create();
 
@@ -38,7 +64,7 @@ class User
      * - Showing the logged in user form (for normal users)
      * - Showing a listing of available users (for admins)
      */
-    public static function contextbased($step = 'init')
+    public static function contextbased()
     {
         $user = Login::getUser();
 
@@ -51,7 +77,41 @@ class User
             case !!$user :
                 return self::update();
         } // switch
-
     } // contextbased ();
+
+    /**
+     * Validates the user for insertion into the database
+     */
+    protected static function _validate(\UMS\Models\User $user)
+    {
+        $errors = array();
+
+        // required fields validation
+        if ( !$user->email ) {
+            $errors['email'] = 'Email field is required';
+        } else if ( !filter_var($user->email, FILTER_VALIDATE_EMAIL) ) {
+            $errors['email'] = 'Email is not a valid email adress';
+        } else if ( isset($_POST['email-repeat']) && $_POST['email'] !== $_POST['email-repeat'] ) {
+            $errors['email-repeat'] = 'Email fields do not match';
+        }
+
+        if ( !$user->pass ) {
+            $errors['pass'] = 'Pass field is required';
+        } else if ( isset($_POST['pass']) && strlen($_POST['pass']) < 10 ) {
+            $errors['pass'] = 'Pass field is not long enough';
+        }
+
+        if ( !$user->firstname ) {
+            $errors['firstname'] = 'Firstname is required';
+        }
+
+        if ( !$user->lastname ) {
+            $errors['lastname'] = 'Lastname is required';
+        }
+
+        // TODO: admin check, notes check
+        return $errors;
+    } // _validate();
+
 
 } // end class UMS\Controllers\User
